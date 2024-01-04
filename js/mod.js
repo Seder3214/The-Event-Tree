e477a0378b8311c3c8f44aa4e7230bd101d9e2cc
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2024-01-04 14:57:32 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -24,7 +24,7 @@ let winText = `Congratulations! You have reached the end and beaten this game, b
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
-var doNotCallTheseFunctionsEveryTick = ["blowUpEverything"]
+var doNotCallTheseFunctionsEveryTick = ["blowUpEverything","getBgColor","getBdColor"]
 
 function getStartPoints(){
     return new Decimal(modInfo.initialStartPoints)
